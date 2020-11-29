9a62a46b793e1e51817926bc75979442d4b9cf58
FoggyFinder <FoggyFinder@yandex.ua>
2020-11-29 10:36:02 +0200

    apply workaround

--- CoreRtAvaloniaMCVE/App.fs
+++ CoreRtAvaloniaMCVE/App.fs
@@ -8,7 +8,6 @@ let buildAvaloniaApp () = AppBuilder.Configure<Views.App>().UsePlatformDetect()
 
 let app () = buildAvaloniaApp().SetupWithoutStarting().Instance
 
-[<STAThread>]
 [<EntryPoint>]
 let main _ = 
     Framework.RunApplication (Navigation.singleView app Views.MainWindow, Program.applicationCore)
